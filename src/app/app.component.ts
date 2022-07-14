c0d3e2807c8caced282f42eeb05cf0e9d2951b43
vvkjava <vamshivkjava@gmail.com>
2022-07-14 00:05:08 -0400

    Chipus Pizza changes lab work

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { MenuItem } from './interface/menu-item';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'menu';
+  // title = 'menu';
+  title : string = 'Welcome to Chirpus Pizza!';
+
+  pizzas: MenuItem[] = [
+    {item: "Chicken Fingers", category: "dinner", price: 11.99},
+    {item: "Pizza", category: "dinner", price: 11.99},
+    {item: "Wings", category: "sides", price: 8.99},
+    {item: "BreadSticks", category: "sides", price: 4.99},
+    {item: "Ceasar Salad", category: "salads", price: 5.99},
+    {item: "Cinnamon Roll", category: "dessert", price: 8.99},
+    {item: "Sprite", category: "beverages", price: 2.59},
+    {item: "Coke", category: "beverages", price: 2.99}
+
+  ]
+
+  singleItem: MenuItem = {item: "Coke", category: "beverages", price: 2.99}
+
+  trackMenu(index: number, menu: MenuItem) {
+    console.log("Track by Chirpus pizza " + menu.item);
+    return menu.item;
+  }
+
+
 }
